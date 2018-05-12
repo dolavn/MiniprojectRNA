a691d21f6d5bce2ee712800cd1bbb386b833f17b
Tal <tal.lavon1@gmail.com>
2018-05-12 17:27:59 +0300

    added a class that reconstruct the rna structure from the DP matrix

--- Algorithm.py
+++ Algorithm.py
@@ -1,26 +1,19 @@
 class DPMatrix:
     def __init__(self, sequence):
         self.sequence = sequence
-        self.dp_table = [[0 for x in range(len(sequence))] for x in range(len(sequence))]
+        self.seq_len = len(sequence)
+        self.dp_table = [[0 for x in range(self.seq_len)] for x in range(self.seq_len)]
 
     def rna_folding(self):
-        seq_len = len(self.sequence)
-        print(self.dp_table)
-        it = 1
-        for j in range(seq_len-1):
-            for i in range(seq_len-1):
+        for j in range(self.seq_len-1):
+            for i in range(self.seq_len-1):
                 j += 1
-                if j < seq_len:
+                if j < self.seq_len:
                     print('i = ', i)
                     print('j = ', j)
-                    a = (self.sequence[i] == 'A' and self.sequence[j] == 'U')
-                    b = (self.sequence[i] == 'U' and self.sequence[j] == 'A')
-                    c = (self.sequence[i] == 'C' and self.sequence[j] == 'G')
-                    d = (self.sequence[i] == 'G' and self.sequence[j] == 'C')
                     val1 = 0
-                    print('a: ', a, ' b: ', b, ' c: ', c, ' d :', d)
-                    if a or b or c or d:
-                        val1 = self.dp_table[i + 1][j - 1] + 1
+                    if self.base_pair(i, j):
+                        val1 = self.dp_table[i+1][j-1] + 1
                     print('val1: ', val1)
                     val2 = 0
                     for k in range(i, j):
@@ -30,14 +23,50 @@ class DPMatrix:
                     print('val2: ', val2)
                     max_val = max(val1, val2)
                     self.dp_table[i][j] = max_val
-                it += 1
         print(self.dp_table)
-        return self.dp_table[0][seq_len-1]
+        return self.dp_table[0][self.seq_len-1]
 
     def get_matrix(self):
         return self.dp_table
 
-seq = "AGCUAU"
+    def base_pair(self, i, j):
+        a = (self.sequence[i] == 'A' and self.sequence[j] == 'U')
+        b = (self.sequence[i] == 'U' and self.sequence[j] == 'A')
+        c = (self.sequence[i] == 'C' and self.sequence[j] == 'G')
+        d = (self.sequence[i] == 'G' and self.sequence[j] == 'C')
+        return a or b or c or d
+
+    def get_rna_structure(self):
+        stack = []
+        pairs = []
+        stack.append([0, self.seq_len-1])
+        while len(stack) > 0:
+            curr_pair = stack.pop()
+            i = curr_pair[0]
+            j = curr_pair[1]
+            if i >= j:
+                continue
+            elif self.dp_table[i+1][j] == self.dp_table[i][j]:
+                stack.append([i+1, j])
+            elif self.dp_table[i][j-1] == self.dp_table[i][j]:
+                stack.append([i, j-1])
+            elif self.dp_table[i+1][j-1]+1 == self.dp_table[i][j] and self.base_pair(i, j):
+                stack.append([i+1, j-1])
+                pairs.append([i, j])
+            else:
+                for k in range(i+1, j-1):
+                    if self.dp_table[i][k]+self.dp_table[k+1][j] == self.dp_table[i][j]:
+                        stack.append([i, k])
+                        stack.append([k+1, j])
+                        break
+        return pairs
+
+
+
+#seq = "AGCUAU"
+seq = "GGGAAAUCC"
 matrix = DPMatrix(seq)
 ans = matrix.rna_folding()
 print(ans)
+rna = matrix.get_rna_structure()
+print(rna)
